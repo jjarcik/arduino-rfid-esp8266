663b257ed51123d4c659103c7b8c8ead4173afdc
Jan Jarčík <jarcik.jan@gmail.com>
2017-01-11 02:48:45 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,84 +1,110 @@
-#include <SoftwareSerial.h>
- 
-#define DEBUG true
- 
-SoftwareSerial esp8266(2,3); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
-                             // This means that you need to connect the TX line from the esp to the Arduino's pin 2
-                             // and the RX line from the esp to the Arduino's pin 3
+#include "WiFiEsp.h"
+
+// Emulate Serial1 on pins 6/7 if not present
+#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
+SoftwareSerial Serial1(6, 7); // RX, TX
+#endif
+
+SoftwareSerial RFID(2, 3); // RX and TX
+
+char ssid[] = "WIFI";            // your network SSID (name)
+char pass[] = "PASSWORD";        // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+char server[] = "arduino.cc";
+int i;
+
+// Initialize the Ethernet client object
+WiFiEspClient client;
+
 void setup()
 {
+  // initialize serial for debugging
   Serial.begin(9600);
-  esp8266.begin(9600); // your esp's baud rate might be different
-  
-  
-  sendData("AT+RST\r\n",5000,DEBUG); // reset module
-  sendData("AT+CWMODE=3\r\n",5000,DEBUG); // configure as access point  
-  sendData("AT+CIPMUX=0\r\n",1000,DEBUG); // configure for multiple connections  
-  Serial.println("wait 15 seconds");
-  sendData("AT+CWJAP=\"SSID WIFI\",\"password\"\r\n",15000,DEBUG); // get ip address  
-  sendData("AT+CIFSR\r\n",5000,DEBUG); // get ip address
+  // initialize serial for ESP module
+  Serial1.begin(9600);
+  // initialize ESP module
+  WiFi.init(&Serial1);
 
-  sendData("AT+CIPSTART=\"TCP\",\"10.0.0.2\",3000\r\n",3000,DEBUG); // connect to webpage
-  sendData("AT+CIPSEND=36\r\n",3000,DEBUG); // init sending
-  sendData("GET /ok HTTP/1.1\r\n",3000,DEBUG); // send header
-  sendData("Host: 10.0.0.2\r\n",3000,DEBUG); // send data
-  sendData("\r\n",3000,DEBUG); // send data
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
+
+  // you're connected now, so print out the data
+  Serial.println("You're connected to the network");
+  
+  printWifiStatus();
+
+  Serial.println();
+  Serial.println("Starting connection to server...");
+  // if you get a connection, report back via serial
+  if (client.connect(server, 80)) {
+    Serial.println("Connected to server");
+    // Make a HTTP request
+    client.println("GET /asciilogo.txt HTTP/1.1");
+    client.println("Host: arduino.cc");
+    client.println("Connection: close");
+    client.println();
+  }
+
+  RFID.begin(9600); // start serial to RFID reader
 }
- 
+
 void loop()
 {
-  
-  if(esp8266.available()) // check if the esp is sending a message 
-  {
-    while(esp8266.available())
-    {
-      // The esp has data so display its output to the serial window 
-      char c = esp8266.read(); // read the next character.
-      Serial.write(c);
-    }
+  // if there are incoming bytes available
+  // from the server, read them and print them
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
 
-  /*
-    Serial.println("AT+CIPSEND=0,5");
-    sendData("AT+CIPSEND=0,5\r\n",1000,DEBUG);  
-    Serial.println("hello");
-    sendData("hello\r\n",1000,DEBUG);  
-    Serial.println("AT+CIPCLOSE=0");
-    sendData("AT+CIPCLOSE=0\r\n",1000,DEBUG);
-    /**/
+  if (RFID.available() > 0) {
+    while (RFID.available()) {
+      i = RFID.read();
+      Serial.print(i, DEC);    
+    }
+}
+
+  // if the server's disconnected, stop the client
+  
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("Disconnecting from server...");
+    client.stop();
+
+    // do nothing forevermore
+    //while (true);
   }
 }
- 
-/*
-* Name: sendData
-* Description: Function used to send data to ESP8266.
-* Params: command - the data/command to send; timeout - the time to wait for a response; debug - print to Serial window?(true = yes, false = no)
-* Returns: The response from the esp8266 (if there is a reponse)
-*/
-String sendData(String command, const int timeout, boolean debug)
-{
 
-  Serial.println(command);
-    String response = "";
-    
-    esp8266.print(command); // send the read character to the esp8266
-    
-    long int time = millis();
-    
-    while( (time+timeout) > millis())
-    {
-      while(esp8266.available())
-      {
-        
-        // The esp has data so display its output to the serial window 
-        char c = esp8266.read(); // read the next character.
-        response+=c;
-      }  
-    }
-    
-    if(debug)
-    {
-      Serial.print(response);
-    }
-    
-    return response;
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
 }
